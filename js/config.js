9325a5291b8dff679ade2199a3653e74b7652228
m6curly <m6curly@gmail.com>
2026-08-25 00:35:39 +0530

    Enable Razorpay live payments

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.NOOLTHARI_CONFIG = {
   SUPABASE_URL: 'https://moptzrldmubkfteemyql.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_3ZkWBY7bMvEjEMgyrA_i6g_6Coz5GF7',
   SUPABASE_ANON_KEY: '',
-  RAZORPAY_KEY_ID: 'rzp_test_REPLACE_ME',
+  RAZORPAY_KEY_ID: 'rzp_live_TThhl9dpqxZEc6',
   PAYMENT_FUNCTION_CREATE: 'create-razorpay-order',
   PAYMENT_FUNCTION_VERIFY: 'verify-razorpay-payment',
   SITE_URL: window.location.origin
